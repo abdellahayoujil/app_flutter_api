1047c8a0aa2b999eaabad15fc465cd5bc2ca340d
abdellahcodes <ayoujil.abdellah3@gmail.com>
2025-02-17 18:11:17 +0100

    Fix CaractersCubit and CaractersState logic

--- lib/logic/cubit/caracters_cubit.dart
+++ lib/logic/cubit/caracters_cubit.dart
@@ -1,31 +1,24 @@
 import 'package:bloc/bloc.dart';
-<<<<<<< HEAD
 import 'package:meta/meta.dart';
-=======
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 import 'package:rickandmorty/data/model/Character.dart';
 import 'package:rickandmorty/data/repo/Characters_repo.dart';
->>>>>>> ac4edbd (Complete code of Cubit and Dio)
 
 part 'caracters_state.dart';
 
 class CaractersCubit extends Cubit<CaractersState> {
-<<<<<<< HEAD
-  CaractersCubit() : super(CaractersInitial());
-=======
+  final CharactersRepo charactersRepo;
 
-  final CharactersRepo charactersrepo;
-  late List<Character> characters;
-  
-  CaractersCubit(this.charactersrepo) : super(CaractersInitial());
+  CaractersCubit(this.charactersRepo) : super(CaractersInitial());
 
-  List<Character> getCharacters(){
-    charactersrepo.getCharacters().then((Characters) {
-      emit(CaractersLoaded(Characters));
-      this.characters = Characters;
+  List<Character> characters = [];
+
+  void getCharacters() {
+    charactersRepo.getCharacters().then((charactersList) {
+      characters = charactersList;
+      emit(CaractersLoaded(characters));
+    }).catchError((error) {
+      print("Error fetching characters: $error");
     });
-    return characters;
   }
->>>>>>> ac4edbd (Complete code of Cubit and Dio)
 }
--- lib/logic/cubit/caracters_state.dart
+++ lib/logic/cubit/caracters_state.dart
@@ -4,12 +4,9 @@ part of 'caracters_cubit.dart';
 sealed class CaractersState {}
 
 final class CaractersInitial extends CaractersState {}
-<<<<<<< HEAD
-=======
 
 class CaractersLoaded extends CaractersState{
   final List<Character> characters;
 
   CaractersLoaded(this.characters);
 }
->>>>>>> ac4edbd (Complete code of Cubit and Dio)
